0f29c79490f30dd1d7082cb3f7d570a72438bea9
e1um <ericr.lum@gmail.com>
2018-06-07 12:11:18 -0400

    Final Project

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://eric:eric@ds053688.mlab.com:53688/devconnector',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
